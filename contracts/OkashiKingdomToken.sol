84cc702d60fe06f8efe110deab2875c41dcf6d35
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-12-06 02:47:29 +0900

    クラウドセールする

--- contracts/OkashiKingdomToken.sol
+++ contracts/OkashiKingdomToken.sol
@@ -4,10 +4,5 @@ import "zeppelin-solidity/contracts/token/MintableToken.sol";
 contract OkashiKingdomToken is MintableToken {
   string public name = "OkashiKingdomToken";
   string public symbol = "OKT";
-  uint public decimals = 18;
-
-  function OkashiKingdomToken(uint initialSupply) public {
-    totalSupply = initialSupply;
-    balances[msg.sender] = initialSupply;
-  }
+  uint256 public decimals = 18;
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,10 @@
-var OkashiKingdomToken = artifacts.require("./OkashiKingdomToken.sol");
+var OkashiKingdomTokenCrowdsale = artifacts.require("./OkashiKingdomTokenCrowdsale.sol");
 
-module.exports = function(deployer) {
-  let initialSupply = 50000e18
-  deployer.deploy(OkashiKingdomToken, initialSupply);
+module.exports = function(deployer, network, accounts) {
+  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1
+  const endTime = startTime + (86400 * 20)
+  const rate = new web3.BigNumber(1000)
+  const wallet = accounts[0]
+
+  deployer.deploy(OkashiKingdomTokenCrowdsale, startTime, endTime, rate, wallet)
 };
