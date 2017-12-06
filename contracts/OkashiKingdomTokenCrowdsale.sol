70b7a45412b498f40bb8cb6a89d398cb54a9fc75
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-12-06 21:49:08 +0900

    test

--- contracts/OkashiKingdomTokenCrowdsale.sol
+++ contracts/OkashiKingdomTokenCrowdsale.sol
@@ -4,13 +4,14 @@ import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
 
 contract OkashiKingdomTokenCrowdsale is Crowdsale {
 
-  //function OkashiKingdomTokenCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet)
-  //  Crowdsale(_startTime, _endTime, _rate, _wallet) {
-  //}
+  function OkashiKingdomTokenCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet)
+    Crowdsale(_startTime, _endTime, _rate, _wallet)
+    {
+    }
 
   // creates the token to be sold.
   // override this method to have crowdsale of a specific MintableToken token.
-  //function createTokenContract() internal returns (MintableToken) {
-  //  return new OkashiKingdomToken();
-  //}
+  function createTokenContract() internal returns (MintableToken) {
+    return new OkashiKingdomToken();
+  }
 }
