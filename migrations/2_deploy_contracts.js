65a0b15b856a3da7cf44f70849b09c0cf66a8519
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2017-12-06 21:18:40 +0900

    一旦コミットしておくでやんす

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,11 @@
 var OkashiKingdomTokenCrowdsale = artifacts.require("./OkashiKingdomTokenCrowdsale.sol");
 
 module.exports = function(deployer, network, accounts) {
-  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1
-  const endTime = startTime + (86400 * 20)
-  const rate = new web3.BigNumber(1000)
-  const wallet = accounts[0]
+//  const startTime = web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1
+//  const endTime = startTime + (86400 * 20)
+//  const rate = new web3.BigNumber(1000)
+//  const wallet = accounts[0]
 
-  deployer.deploy(OkashiKingdomTokenCrowdsale, startTime, endTime, rate, wallet)
+//  deployer.deploy(OkashiKingdomTokenCrowdsale, startTime, endTime, rate, wallet)
+  deployer.deploy(OkashiKingdomTokenCrowdsale);
 };
--- truffle.js
+++ truffle.js
@@ -1,5 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = process.env.ROPSTEN_MNEMONIC;
+var accessToken = process.env.INFURA_ACCESS_TOKEN;
+
 module.exports = {
   networks: {
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/" + accessToken
+        );
+      },
+      network_id: 3,
+      gas: 4600000
+    },
     development: {
       host: "localhost",
       port: 8545,
